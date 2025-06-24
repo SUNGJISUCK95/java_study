0c76b5f3b91af70c0dbdf01e518b00b31106a860
SUNGJISUCK95 <ebah1111@gmail.com>
2025-06-24 16:50:55 +0900

    java_study commit

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class Hello {
+	//main method
+	public static void main(String[] args) {
+		System.out.print("Welcome to Java"); 
+		//print는 내려쓰기 안됨
+		System.out.println("Hello");
+		//sysout + ctrl + space (System.out.println 자동완성)
+	}
+}
